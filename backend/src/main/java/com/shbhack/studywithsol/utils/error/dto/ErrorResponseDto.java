775b232f5d983ff62ebcc157f4fe8bb68971ba68
yeonhee <ydol2@naver.com>
2023-09-03 12:37:14 +0900

    Feat: 예외처리 커스텀 구현

---
+++ backend/src/main/java/com/shbhack/studywithsol/utils/error/dto/ErrorResponseDto.java
@@ -0,0 +1,7 @@
+package com.shbhack.studywithsol.utils.error.dto;
+
+public record ErrorResponseDto(int code, String phrase) {
+    public static ErrorResponseDto of(int code,String phrase){
+        return new ErrorResponseDto(code, phrase);
+    }
+}
