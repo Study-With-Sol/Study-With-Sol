925ced45c9544e488d907b21465851ec180aecf0
yeonx <71878202+yeonx@users.noreply.github.com>
2023-09-03 11:52:08 +0900

    Feat: 백엔드 초기 설정
    스프링 버전 '2.7.15'
    자바 버전 '17'

---
+++ backend/src/main/java/com/shbhack/studywithsol/StudywithsolApplication.java
@@ -0,0 +1,13 @@
+package com.shbhack.studywithsol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudywithsolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudywithsolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shbhack/studywithsol/StudywithsolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shbhack.studywithsol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudywithsolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
