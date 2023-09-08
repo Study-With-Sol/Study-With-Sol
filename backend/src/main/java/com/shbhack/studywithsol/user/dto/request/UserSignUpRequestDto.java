74597532872b76f890e9abef1d590862e5fdb15d
An Na <99172832+An0401na@users.noreply.github.com>
2023-09-09 03:04:39 +0900

    Fix: UserSignUpRequestDto 수정

--- backend/src/main/java/com/shbhack/studywithsol/user/dto/request/UserSignUpRequestDto.java
+++ backend/src/main/java/com/shbhack/studywithsol/user/dto/request/UserSignUpRequestDto.java
@@ -1,24 +1,29 @@
 package com.shbhack.studywithsol.user.dto.request;
 
+import com.shbhack.studywithsol.user.domain.User;
 import com.sun.istack.NotNull;
+import lombok.Builder;
 
 import javax.persistence.Column;
 
+@Builder
 public record UserSignUpRequestDto(
         @NotNull
-        Boolean isParent,
-
-        @NotNull
-        String nickname,
+        String id,
 
         @NotNull
         String password,
+
         @NotNull
         String name,
-        @NotNull
-        String email,
 
-        String phoneNumber
+        String phoneNumber,
+
+        @NotNull
+        Boolean isParent
 
 ) {
+    public User toUser(){
+        return new User(id, password, name, phoneNumber, isParent);
+    }
 }
