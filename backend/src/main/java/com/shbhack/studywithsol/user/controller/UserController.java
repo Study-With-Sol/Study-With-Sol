96aaa5bfddfe9843439a3503325a108c4294b591
An Na <99172832+An0401na@users.noreply.github.com>
2023-09-09 03:06:42 +0900

    Feat: 1원이체, 본인인증, 회원가입 구현 중

--- backend/src/main/java/com/shbhack/studywithsol/user/controller/UserController.java
+++ backend/src/main/java/com/shbhack/studywithsol/user/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.shbhack.studywithsol.user.controller;
 
+import com.shbhack.studywithsol.user.dto.request.UserAuthenticationRequestDto;
+import com.shbhack.studywithsol.user.dto.request.UserOneTransferRequestDto;
+import com.shbhack.studywithsol.user.dto.request.UserSignUpRequestDto;
 import com.shbhack.studywithsol.user.service.UserService;
+import com.shbhack.studywithsol.utils.dto.response.BaseResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+
+    /*
+     1원 이체
+     */
+    @PostMapping("/1transfer")
+    public BaseResponseDto oneTransfer(@RequestBody UserOneTransferRequestDto userOneTransferRequestDto){
+        return BaseResponseDto.ok(userService.oneTransfer(userOneTransferRequestDto));
+    }
+
+    /*
+    1원 이체로 본인 인증
+     */
+    @PostMapping("/authentication")
+    public BaseResponseDto authentication(@RequestBody UserAuthenticationRequestDto userAuthenticationRequestDto){
+//        return BaseResponseDto.ok(userService.authentication(userAuthenticationRequestDto));
+        return null;
+    }
+    @PostMapping("/sign-up")
+    public BaseResponseDto signUp(@RequestBody UserSignUpRequestDto userSignUpRequestDto){
+        return BaseResponseDto.ok(userService.signUp(userSignUpRequestDto));
+    }
+
 
 }
