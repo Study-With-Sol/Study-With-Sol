aa9e0c3b895a3e451d826dc3daf4d43017b48276
An Na <99172832+An0401na@users.noreply.github.com>
2023-09-09 03:05:15 +0900

    Feat: 웹 클라이언트 추가

--- backend/src/main/java/com/shbhack/studywithsol/user/service/UserService.java
+++ backend/src/main/java/com/shbhack/studywithsol/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.shbhack.studywithsol.user.service;
 
+import com.shbhack.studywithsol.user.dto.request.UserOneTransferRequestDto;
+import com.shbhack.studywithsol.user.dto.request.UserSignUpRequestDto;
 import com.shbhack.studywithsol.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.transaction.Transactional;
 
@@ -10,7 +13,17 @@ import javax.transaction.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class UserService {
+
+    WebClient webClient = WebClient
+            .builder().baseUrl("https://shbhack.shinhan.com").build();
+
     private final UserRepository userRepository;
+    public Object signUp(UserSignUpRequestDto userSignUpRequestDto) {
+        //id 중복 확인
+        return null;
+    }
 
+    public Object oneTransfer(UserOneTransferRequestDto userOneTransferRequestDto) {
 
+    }
 }
