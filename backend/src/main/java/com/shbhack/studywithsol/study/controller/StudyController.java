c28a5ba8ba65af9efe2c32dacd9610c0bb6e5931
rladbwls30 <rladbwls30@naver.com>
2023-09-09 17:50:54 +0900

    Feat: 학습 삭제, 조회 Controller 생성

--- backend/src/main/java/com/shbhack/studywithsol/study/controller/StudyController.java
+++ backend/src/main/java/com/shbhack/studywithsol/study/controller/StudyController.java
@@ -1,13 +1,37 @@
 package com.shbhack.studywithsol.study.controller;
 
-import com.shbhack.studywithsol.study.service.StudyListService;
+import com.shbhack.studywithsol.study.dto.StudyDto;
+import com.shbhack.studywithsol.study.service.StudyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/study")
 public class StudyController {
-    private final StudyListService studyListService;
+    private final StudyService studyService;
+
+
+    @PostMapping("/parent")
+    public ResponseEntity<List<StudyDto.StudyResponseDto>> getChildStudyList(@RequestBody StudyDto.StudyRequestDto studyRequestDto){
+        List<StudyDto.StudyResponseDto> studyList = studyService.getChildStudyList(studyRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(studyList);
+    }
+
+    @PostMapping("/child")
+    public ResponseEntity<List<StudyDto.StudyResponseDto>> getStudyList(@RequestBody StudyDto.StudyRequestDto studyRequestDto){
+        List<StudyDto.StudyResponseDto> studyList = studyService.getStudyList(studyRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(studyList);
+    }
+
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteList(@RequestBody StudyDto.StudyRequestDto studyRequestDto){
+        String str = studyService.deleteList(studyRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(str);
+    }
 }
