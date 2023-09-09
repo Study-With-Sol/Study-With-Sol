fe627ca7bbe14990741c2d2824e96052760a6af7
An Na <99172832+An0401na@users.noreply.github.com>
2023-09-10 03:19:16 +0900

    Fix:  회원 가입시 중복된 아이디로 가입 시도 예외처리

--- backend/src/main/java/com/shbhack/studywithsol/user/service/UserService.java
+++ backend/src/main/java/com/shbhack/studywithsol/user/service/UserService.java
@@ -5,6 +5,8 @@ import com.shbhack.studywithsol.user.dto.request.UserAuthenticationRequest;
 import com.shbhack.studywithsol.user.dto.request.UserSignUpRequest;
 import com.shbhack.studywithsol.user.dto.response.UserAuthenticationResponse;
 import com.shbhack.studywithsol.user.repository.UserRepository;
+import com.shbhack.studywithsol.utils.error.enums.ErrorMessage;
+import com.shbhack.studywithsol.utils.error.exception.custom.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -25,7 +27,6 @@ public class UserService {
     }
 
     // ID 중복 검사
-
     public Boolean duplicationCheck(String id) {
         if(!userRepository.findById(id).isPresent()){
             return false;
@@ -34,6 +35,11 @@ public class UserService {
     }
 
     public Boolean signUp(UserSignUpRequest userSignUpRequest) {
+        //Id 중복 확인
+        userRepository.findById(userSignUpRequest.id()).ifPresent(
+                user -> { throw new BusinessException(ErrorMessage.USER_ID_DUPLICATED);}
+        );
+
         //저장
         User user = userRepository.save(userSignUpRequest.toUser());
 
--- backend/src/main/java/com/shbhack/studywithsol/utils/error/enums/ErrorMessage.java
+++ backend/src/main/java/com/shbhack/studywithsol/utils/error/enums/ErrorMessage.java
@@ -3,15 +3,17 @@ package com.shbhack.studywithsol.utils.error.enums;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
 
     // 에러 메시지 추가하면 됩니다.
-    USER_NOT_FOUND(NOT_FOUND, "해당 회원을 찾을 수 없습니다.");
+    USER_NOT_FOUND(NOT_FOUND, "해당 회원을 찾을 수 없습니다."),
+    USER_ID_DUPLICATED(CONFLICT,"해당 아이디가 이미 존재합니다.");
 
     private final int code;
     private final String phrase;
