7fdb23de1f6f7e5a57a6889c4031ea82fc106820
An Na <99172832+An0401na@users.noreply.github.com>
2023-09-12 02:40:22 +0900

    Fix: SecurityFilterChain 수정

--- backend/src/main/java/com/shbhack/studywithsol/utils/config/SecurityConfig.java
+++ backend/src/main/java/com/shbhack/studywithsol/utils/config/SecurityConfig.java
@@ -2,12 +2,15 @@ package com.shbhack.studywithsol.utils.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -17,8 +20,9 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
+//                .antMatchers("/**").permitAll()
                 .antMatchers("/users/sign-up", "/users/login").permitAll()
+//                .antMatchers(HttpMethod.POST, "/ttest").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)//jwt 사용할때 씀
