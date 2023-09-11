d34f274b3c178f5a456418ea1e83be904d07fed9
yeonhee <ydol2@naver.com>
2023-09-11 19:24:55 +0900

    Feat: 거래내역 조회 API 구현

--- backend/src/main/java/com/shbhack/studywithsol/transaction/controller/TransactionController.java
+++ backend/src/main/java/com/shbhack/studywithsol/transaction/controller/TransactionController.java
@@ -1,10 +1,15 @@
 package com.shbhack.studywithsol.transaction.controller;
 
 import com.shbhack.studywithsol.transaction.dto.request.TransactionCreateRequest;
+import com.shbhack.studywithsol.transaction.dto.request.TransactionListReadRequest;
 import com.shbhack.studywithsol.transaction.dto.response.TransactionCreateResponse;
+import com.shbhack.studywithsol.transaction.dto.response.TransactionListReadResponse;
 import com.shbhack.studywithsol.transaction.service.TransactionService;
 import com.shbhack.studywithsol.utils.dto.response.BaseResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +27,12 @@ public class TransactionController {
     @PostMapping
     public BaseResponseDto<TransactionCreateResponse> save(@RequestBody @Valid TransactionCreateRequest request) {
         return BaseResponseDto.ok(transactionService.save(request));
+    }
+
+    @GetMapping
+    public BaseResponseDto<Slice<TransactionListReadResponse>> getTransactionList(@RequestBody @Valid TransactionListReadRequest request,
+                                                                                  Pageable pageable) {
+        return BaseResponseDto.ok(transactionService.getTransactionList(request.accountId(), pageable));
     }
 
 }
