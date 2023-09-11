85681dcb9e7ba16edb4f948fb44611b5fa7a5aad
yeonhee <ydol2@naver.com>
2023-09-11 19:15:50 +0900

    Feat: 거래 내역 로직 구현

--- backend/src/main/java/com/shbhack/studywithsol/transaction/service/TransactionService.java
+++ backend/src/main/java/com/shbhack/studywithsol/transaction/service/TransactionService.java
@@ -3,8 +3,11 @@ package com.shbhack.studywithsol.transaction.service;
 import com.shbhack.studywithsol.transaction.domain.Transaction;
 import com.shbhack.studywithsol.transaction.dto.request.TransactionCreateRequest;
 import com.shbhack.studywithsol.transaction.dto.response.TransactionCreateResponse;
+import com.shbhack.studywithsol.transaction.dto.response.TransactionListReadResponse;
 import com.shbhack.studywithsol.transaction.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,5 +23,14 @@ public class TransactionService {
         Transaction transaction = transactionRepository.save(request.toEntity());
 
         return TransactionCreateResponse.from(transaction);
+    }
+
+    @Transactional(readOnly = true)
+    public Slice<TransactionListReadResponse> getTransactionList(Long accountId, Pageable pageable){
+
+        Slice<TransactionListReadResponse> slice = transactionRepository
+                .getTransactionList(accountId, pageable);
+
+        return slice;
     }
 }
