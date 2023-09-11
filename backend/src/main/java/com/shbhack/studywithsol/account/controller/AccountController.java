55b636ef79b7fc3933d36b167078533aa0b0dd5c
yeonhee <ydol2@naver.com>
2023-09-11 20:37:56 +0900

    Feat: 계좌 등록 API구현
    로그인 유저 정보 연결 필요

--- backend/src/main/java/com/shbhack/studywithsol/account/controller/AccountController.java
+++ backend/src/main/java/com/shbhack/studywithsol/account/controller/AccountController.java
@@ -1,8 +1,10 @@
 package com.shbhack.studywithsol.account.controller;
 
 import com.shbhack.studywithsol.account.dto.request.AccountCreateRequest;
+import com.shbhack.studywithsol.account.dto.request.AccountRegistrationRequest;
 import com.shbhack.studywithsol.account.dto.response.AccountCreateResponse;
 import com.shbhack.studywithsol.account.dto.response.AccountReadResponse;
+import com.shbhack.studywithsol.account.dto.response.AccountRegistrationResponse;
 import com.shbhack.studywithsol.account.service.AccountService;
 import com.shbhack.studywithsol.utils.dto.response.BaseResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -17,15 +19,21 @@ public class AccountController {
 
     private final AccountService accountService;
 
-    @PostMapping("/creation")
-    public BaseResponseDto<AccountCreateResponse> save(@RequestBody @Valid AccountCreateRequest request) {
-        return BaseResponseDto.ok(accountService.save(request));
+    //User 설정 후 변경 필요
+    @PostMapping
+    public BaseResponseDto<AccountRegistrationResponse> registration(@RequestBody @Valid AccountRegistrationRequest request) {
+
+        return BaseResponseDto.ok(accountService.registration(request, 1L));
     }
 
-    // 유저 연결 필요.
     @GetMapping("/{accountId}")
     public BaseResponseDto<AccountReadResponse> getAccount(@PathVariable Long accountId) {
         return BaseResponseDto.ok(accountService.getAccount(accountId));
+    }
+
+    @PostMapping("/creation")
+    public BaseResponseDto<AccountCreateResponse> save(@RequestBody @Valid AccountCreateRequest request) {
+        return BaseResponseDto.ok(accountService.save(request));
     }
 
 }
