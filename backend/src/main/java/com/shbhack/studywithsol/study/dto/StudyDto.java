037877b3ad7ede75d90607da2e255bbb24d90383
rladbwls30 <rladbwls30@naver.com>
2023-09-11 22:03:48 +0900

    Feat: 학습 등록 위한 Request DTO 생성

--- backend/src/main/java/com/shbhack/studywithsol/study/dto/StudyDto.java
+++ backend/src/main/java/com/shbhack/studywithsol/study/dto/StudyDto.java
@@ -1,18 +1,24 @@
 package com.shbhack.studywithsol.study.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.shbhack.studywithsol.study.domain.Study;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class StudyDto {
 
     @Getter
     @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class StudyRequestDto{
         @NotNull
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
@@ -25,21 +31,41 @@ public class StudyDto {
         private Long parentId;
     }
 
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Builder
+    public static class RegisterStudyListReqDto{
+        @NotNull
+        private Long childrenId;
+
+        @NotNull
+        private Long parentId;
+
+        @NotNull
+        private String content;
+
+        @NotNull
+        private int payMoney;
+
+        @NotNull
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+        private LocalDate deadline;
+    }
+
     @Getter
     @Builder
     public static class StudyResponseDto{
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-        private LocalDate date;
+        private LocalDate deadline;
         private String content;
-        private String payMoney;
-        private Boolean isLongGoal;
+        private int payMoney;
 
-        public static StudyResponseDto from(LocalDate date, String content, String payMoney, Boolean isLongGoal){
+        public static StudyResponseDto from(Study study){
             return StudyResponseDto.builder()
-                    .date(date)
-                    .content(content)
-                    .payMoney(payMoney)
-                    .isLongGoal(isLongGoal)
+                    .deadline(study.getDeadline())
+                    .content(study.getContent())
+                    .payMoney(study.getPayMoney())
                     .build();
         }
     }
