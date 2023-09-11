7b7982c7e535025a17508ed3e30c44cd51ee790b
rladbwls30 <rladbwls30@naver.com>
2023-09-11 22:04:28 +0900

    Feat: 학습 등록, 삭제 controller 생성

--- backend/src/main/java/com/shbhack/studywithsol/study/controller/StudyController.java
+++ backend/src/main/java/com/shbhack/studywithsol/study/controller/StudyController.java
@@ -1,6 +1,8 @@
 package com.shbhack.studywithsol.study.controller;
 
+import com.shbhack.studywithsol.study.domain.Study;
 import com.shbhack.studywithsol.study.dto.StudyDto;
+import com.shbhack.studywithsol.study.repository.StudyRepository;
 import com.shbhack.studywithsol.study.service.StudyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,24 +16,41 @@ import java.util.List;
 @RequestMapping("/study")
 public class StudyController {
     private final StudyService studyService;
+    private final StudyRepository studyRepository;
 
-
+    // 부모가 해당 자녀에게 등록한 학습 조회
     @PostMapping("/parent")
     public ResponseEntity<List<StudyDto.StudyResponseDto>> getChildStudyList(@RequestBody StudyDto.StudyRequestDto studyRequestDto){
         List<StudyDto.StudyResponseDto> studyList = studyService.getChildStudyList(studyRequestDto);
         return ResponseEntity.status(HttpStatus.OK).body(studyList);
     }
 
+    // 자녀가 가진 모든 학습 조회
     @PostMapping("/child")
     public ResponseEntity<List<StudyDto.StudyResponseDto>> getStudyList(@RequestBody StudyDto.StudyRequestDto studyRequestDto){
         List<StudyDto.StudyResponseDto> studyList = studyService.getStudyList(studyRequestDto);
         return ResponseEntity.status(HttpStatus.OK).body(studyList);
     }
 
+    // 자녀에게 학습 등록
+    @PostMapping
+    public ResponseEntity<StudyDto.StudyResponseDto> registStudyList(@RequestBody StudyDto.RegisterStudyListReqDto registerStudyListReqDto){
+        Study study = Study.from(registerStudyListReqDto);
+        studyRepository.save(study);
+        return ResponseEntity.status(HttpStatus.OK).body(StudyDto.StudyResponseDto.from(study));
+    }
 
+    // 해당 날짜의 자녀 학습 전부 삭제
     @DeleteMapping
     public ResponseEntity<String> deleteList(@RequestBody StudyDto.StudyRequestDto studyRequestDto){
         String str = studyService.deleteList(studyRequestDto);
         return ResponseEntity.status(HttpStatus.OK).body(str);
+    }
+
+    @DeleteMapping("/{studyId}")
+    public ResponseEntity<String> deleteStudyOne(@PathVariable Long studyId){
+        String content = studyRepository.findById(studyId).get().getContent();
+        studyRepository.deleteById(studyId);
+        return ResponseEntity.status(HttpStatus.OK).body(content);
     }
 }
