17a4d1639f0c959ee974833343265ebea24ee752
rladbwls30 <rladbwls30@naver.com>
2023-09-12 19:14:56 +0900

    Fix: RequestMapping으로 수정

--- backend/src/main/java/com/shbhack/studywithsol/study/controller/StudyController.java
+++ backend/src/main/java/com/shbhack/studywithsol/study/controller/StudyController.java
@@ -9,35 +9,33 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping(value = "/study")
+@RequestMapping(value = "/study")
 public class StudyController {
     private final StudyService studyService;
 
     // 자녀에게 학습 등록
-    @PostMapping("/study")
+    @PostMapping
     public ResponseEntity<String> registerStudyList(@RequestBody StudyDto.RegisterStudyListReqDto registerStudyListReqDto){
         String str = studyService.registerStudyList(registerStudyListReqDto);
         return ResponseEntity.status(HttpStatus.OK).body(str);
     }
 
     // 학습 리스트 조회
-    @PostMapping("/study/list")
+    @PostMapping("/list")
     public ResponseEntity<StudyDto.StudyResponseDto> getStudyList(@RequestBody StudyDto.StudyRequestDto studyRequestDto){
-        System.out.println("들어왔다ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ");
         StudyDto.StudyResponseDto studyList = studyService.getStudyList(studyRequestDto);
-        System.out.println("리스트가 문제일까?");
         return ResponseEntity.status(HttpStatus.OK).body(studyList);
     }
 
     // 해당 날짜의 자녀 학습 전부 삭제
-    @DeleteMapping("/study")
+    @DeleteMapping
     public ResponseEntity<String> deleteList(@RequestBody StudyDto.StudyRequestDto studyRequestDto){
         String str = studyService.deleteList(studyRequestDto);
         return ResponseEntity.status(HttpStatus.OK).body(str); // "delete success"
     }
 
     // 해당 아이디 학습 하나 삭제
-    @DeleteMapping("/study/{studyId}")
+    @DeleteMapping("/{studyId}")
     public ResponseEntity<String> deleteOne(@PathVariable Long studyId){
         String content = studyService.deleteOne(studyId);
         return ResponseEntity.status(HttpStatus.OK).body(content+" delete success"); //삭제한 학습의 내용 return
