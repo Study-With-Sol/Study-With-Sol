56364022cb5c4a00a6804827c5fa7c6ab3d71500
yeonhee <ydol2@naver.com>
2023-09-13 01:14:47 +0900

    Feat: 학습 시 용돈 지급 로직 구현

--- backend/src/main/java/com/shbhack/studywithsol/transaction/service/TransactionService.java
+++ backend/src/main/java/com/shbhack/studywithsol/transaction/service/TransactionService.java
@@ -1,12 +1,17 @@
 package com.shbhack.studywithsol.transaction.service;
 
 import com.shbhack.studywithsol.account.domain.Account;
+import com.shbhack.studywithsol.account.dto.response.AccountCreateResponse;
 import com.shbhack.studywithsol.account.repository.AccountRepository;
 import com.shbhack.studywithsol.transaction.domain.Transaction;
 import com.shbhack.studywithsol.transaction.dto.request.TransactionCreateRequest;
 import com.shbhack.studywithsol.transaction.dto.response.TransactionCreateResponse;
 import com.shbhack.studywithsol.transaction.dto.response.TransactionListReadResponse;
 import com.shbhack.studywithsol.transaction.repository.TransactionRepository;
+import com.shbhack.studywithsol.user.domain.Connection;
+import com.shbhack.studywithsol.user.domain.User;
+import com.shbhack.studywithsol.user.repository.ConnectionRepository;
+import com.shbhack.studywithsol.user.repository.UserRepository;
 import com.shbhack.studywithsol.utils.error.enums.ErrorMessage;
 import com.shbhack.studywithsol.utils.error.exception.custom.BusinessException;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +27,8 @@ public class TransactionService {
 
     private final TransactionRepository transactionRepository;
     private final AccountRepository accountRepository;
-
+    private final ConnectionRepository connectionRepository;
+    private final UserRepository userRepository;
 
     public TransactionCreateResponse save(TransactionCreateRequest request) {
 
@@ -34,6 +40,38 @@ public class TransactionService {
         transaction.beTradedIn(account);
 
         return TransactionCreateResponse.from(transaction);
+    }
+
+    public void payStudyMoney(Long connectionId, String content, Long amount) {
+
+        Connection connection = connectionRepository.findById(connectionId)
+                .orElseThrow(() -> new BusinessException((ErrorMessage.CONNECTION_NOT_FOUND)));
+
+        User parent = userRepository.findById(connection.getParent().getUserId())
+                .orElseThrow(() -> new BusinessException((ErrorMessage.USER_NOT_FOUND)));
+
+        User child = userRepository.findById(connection.getChildren().getUserId())
+                .orElseThrow(() -> new BusinessException((ErrorMessage.USER_NOT_FOUND)));
+
+        Account parentAccount = accountRepository.findById(parent.getMainAccount().getId())
+                .orElseThrow(() -> new BusinessException((ErrorMessage.ACCOUNT_NOT_FOUND)));
+
+        Account childAccount = accountRepository.findById(child.getMainAccount().getId())
+                .orElseThrow(() -> new BusinessException((ErrorMessage.ACCOUNT_NOT_FOUND)));
+
+        Transaction parentTransaction = new Transaction(content, amount, false,
+                connection.getChildren().getName(), connection.getParent().getName());
+
+        parentTransaction.beTradedIn(parentAccount);
+
+        transactionRepository.save(parentTransaction);
+
+        Transaction childTransaction = new Transaction(content, amount, true,
+                connection.getChildren().getName(), connection.getParent().getName());
+
+        childTransaction.beTradedIn(childAccount);
+
+        transactionRepository.save(childTransaction);
     }
 
     @Transactional(readOnly = true)
