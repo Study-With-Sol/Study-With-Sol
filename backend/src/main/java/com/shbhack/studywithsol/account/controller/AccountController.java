0be1af54d8610e3e1d7c03d785d891b4a1b5f951
yeonhee <ydol2@naver.com>
2023-09-14 00:32:49 +0900

    Chore: swagger value 설정

--- backend/src/main/java/com/shbhack/studywithsol/account/controller/AccountController.java
+++ backend/src/main/java/com/shbhack/studywithsol/account/controller/AccountController.java
@@ -11,6 +11,7 @@ import com.shbhack.studywithsol.account.dto.response.AccountRegistrationResponse
 import com.shbhack.studywithsol.account.dto.response.AccountTerminationResponse;
 import com.shbhack.studywithsol.account.service.AccountService;
 import com.shbhack.studywithsol.utils.dto.response.BaseResponseDto;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,26 +33,31 @@ public class AccountController {
     private final AccountService accountService;
 
     //User 설정 후 변경 필요
+    @ApiOperation(value ="게좌 등록")
     @PostMapping
     public BaseResponseDto<AccountRegistrationResponse> registration(@RequestBody @Valid AccountRegistrationRequest request) {
         return BaseResponseDto.ok(accountService.registration(request, 1L));
     }
 
+    @ApiOperation(value ="주계좌 수정")
     @PatchMapping
     public BaseResponseDto<AccountMainUpdateResponse> changeMainAccount(@RequestBody @Valid AccountMainUpdateRequest request) {
         return BaseResponseDto.ok(accountService.changeMainAccount(request, 1L));
     }
 
+    @ApiOperation(value ="계좌 등록 해지")
     @DeleteMapping("/{accountId}")
     public BaseResponseDto<AccountTerminationResponse> termination(@RequestBody @Valid AccountTerminationRequest request) {
         return BaseResponseDto.ok(accountService.termination(request, 1L));
     }
 
+    @ApiOperation(value ="계좌 조회")
     @GetMapping("/{accountId}")
     public BaseResponseDto<AccountReadResponse> getAccount(@PathVariable Long accountId) {
         return BaseResponseDto.ok(accountService.getAccount(accountId));
     }
 
+    @ApiOperation(value ="계좌 생성")
     @PostMapping("/creation")
     public BaseResponseDto<AccountCreateResponse> save(@RequestBody @Valid AccountCreateRequest request) {
         return BaseResponseDto.ok(accountService.save(request));
