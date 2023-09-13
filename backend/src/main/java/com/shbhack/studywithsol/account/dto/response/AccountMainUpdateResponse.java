55b8d86afa2d575098dd71d4aa0cdb4102e1a992
yeonhee <ydol2@naver.com>
2023-09-13 23:57:18 +0900

    Feat: 주계좌 수정 response dto 구현

---
+++ backend/src/main/java/com/shbhack/studywithsol/account/dto/response/AccountMainUpdateResponse.java
@@ -0,0 +1,12 @@
+package com.shbhack.studywithsol.account.dto.response;
+
+public record AccountMainUpdateResponse(
+
+        /**
+         *  id : "계좌PK"
+         */
+
+        Long accountId
+        
+) {
+}
