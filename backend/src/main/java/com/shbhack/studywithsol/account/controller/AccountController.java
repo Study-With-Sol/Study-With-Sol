86a69cab7989f4e0e32a7a3c3d32032ee1ce0fd8
yeonhee <ydol2@naver.com>
2023-09-15 00:28:09 +0900

    Feat: 전체 계좌 조회 API

--- backend/src/main/java/com/shbhack/studywithsol/account/controller/AccountController.java
+++ backend/src/main/java/com/shbhack/studywithsol/account/controller/AccountController.java
@@ -8,6 +8,7 @@ import com.shbhack.studywithsol.account.dto.request.AccountTerminationRequest;
 import com.shbhack.studywithsol.account.dto.response.AccountCreateResponse;
 import com.shbhack.studywithsol.account.dto.response.AccountMainUpdateResponse;
 import com.shbhack.studywithsol.account.dto.response.AccountMainBalanceReadResponse;
+import com.shbhack.studywithsol.account.dto.response.AccountListReadResponse;
 import com.shbhack.studywithsol.account.dto.response.AccountReadResponse;
 import com.shbhack.studywithsol.account.dto.response.AccountRegistrationResponse;
 import com.shbhack.studywithsol.account.dto.response.AccountTerminationResponse;
@@ -15,6 +16,8 @@ import com.shbhack.studywithsol.account.service.AccountService;
 import com.shbhack.studywithsol.utils.dto.response.BaseResponseDto;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -68,6 +71,12 @@ public class AccountController {
     @PostMapping("/creation")
     public BaseResponseDto<AccountCreateResponse> save(@RequestBody @Valid AccountCreateRequest request) {
         return BaseResponseDto.ok(accountService.save(request));
+    }
+
+    @ApiOperation(value ="전체 계좌 조회")
+    @GetMapping("/list")
+    public BaseResponseDto<Slice<AccountListReadResponse>> getAccountList(Pageable pageable) {
+        return BaseResponseDto.ok(accountService.getAccountList(1L, pageable));
     }
 
 }
