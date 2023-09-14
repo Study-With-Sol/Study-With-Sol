39dac0b1f883ec3c7e7d5b017b656f7912646b95
rladbwls30 <rladbwls30@naver.com>
2023-09-14 21:00:40 +0900

    Feat: 에러메세지 추가

--- backend/src/main/java/com/shbhack/studywithsol/utils/error/enums/ErrorMessage.java
+++ backend/src/main/java/com/shbhack/studywithsol/utils/error/enums/ErrorMessage.java
@@ -20,7 +20,9 @@ public enum ErrorMessage {
 
     STUDY_NOT_FOUNT(NOT_FOUND, "해당 학습을 찾을 수 없습니다."),
     STUDY_ALREADY_APPROVAL(NOT_ACCEPTABLE, "지급 완료된 학습은 취소할 수 없습니다."),
-    STUDY_IS_NOT_DONE(NOT_ACCEPTABLE, "완료 되지 않은 학습에 대해서는 지급할 수 없습니다.");
+    STUDY_IS_NOT_DONE(NOT_ACCEPTABLE, "완료 되지 않은 학습에 대해서는 지급할 수 없습니다."),
+
+    NO_ENOUGH_INFORMATION(NOT_ACCEPTABLE, "부모 정보를 전달해 주세요.");
 
     private final int code;
     private final String phrase;
