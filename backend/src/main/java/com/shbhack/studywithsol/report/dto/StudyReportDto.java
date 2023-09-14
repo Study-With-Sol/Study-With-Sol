903d1e0c1e23b41395e6a0c258c21ea1850b2302
rladbwls30 <rladbwls30@naver.com>
2023-09-14 21:02:50 +0900

    Feat: 학습 통계 API를 위한 Request, Response DTO 생성

---
+++ backend/src/main/java/com/shbhack/studywithsol/report/dto/StudyReportDto.java
@@ -0,0 +1,46 @@
+package com.shbhack.studywithsol.report.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+public class StudyReportDto {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class RequestDto{
+        @NotNull
+        private Long childrenId;
+        @NotNull
+        private LocalDate startDate;
+        @NotNull
+        private LocalDate endDate;
+
+        private Long parentId;
+    }
+
+    @Getter
+    @Builder
+    public static class ResponseDto{
+        private int settingStudy;
+        private int completeStudy;
+        private int allowance;
+        private String time;
+
+        public static ResponseDto from(
+                int settingStudy, int completeStudy, int allowance, String time){
+            return ResponseDto.builder()
+                    .settingStudy(settingStudy)
+                    .completeStudy(completeStudy)
+                    .allowance(allowance)
+                    .time(time)
+                    .build();
+        }
+    }
+}
