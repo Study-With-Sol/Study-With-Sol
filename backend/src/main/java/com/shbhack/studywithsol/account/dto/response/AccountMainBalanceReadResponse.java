2af7951b89163aca556b57f6ab4a067e689d73cf
yeonhee <ydol2@naver.com>
2023-09-14 22:16:10 +0900

    Feat: 주계좌 잔액 response dto 생성

---
+++ backend/src/main/java/com/shbhack/studywithsol/account/dto/response/AccountMainBalanceReadResponse.java
@@ -0,0 +1,28 @@
+package com.shbhack.studywithsol.account.dto.response;
+
+public record AccountMainBalanceReadResponse(
+
+        /**
+         * id : 주계좌 PK
+         * name: 주계좌 이름
+         * balance : 주계좌 잔액
+         */
+        
+        Long id,
+        String name,
+        Long balance
+) {
+
+    public static AccountMainBalanceReadResponse from(
+            Long id,
+            String name,
+            Long balance
+    ){
+        return new AccountMainBalanceReadResponse(
+                id,
+                name,
+                balance
+        );
+    }
+
+}
