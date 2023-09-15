2d9e1354924d0e0dd2b818802ec9138cfaa649e5
yeonhee <ydol2@naver.com>
2023-09-16 00:31:45 +0900

    Feat: 용돈 수정 API

--- backend/src/main/java/com/shbhack/studywithsol/pocketmoney/controller/PocketMoneyController.java
+++ backend/src/main/java/com/shbhack/studywithsol/pocketmoney/controller/PocketMoneyController.java
@@ -2,13 +2,16 @@ package com.shbhack.studywithsol.pocketmoney.controller;
 
 import com.shbhack.studywithsol.pocketmoney.dto.request.PocketMoneyCreateRequest;
 import com.shbhack.studywithsol.pocketmoney.dto.request.PocketMoneyReadRequest;
+import com.shbhack.studywithsol.pocketmoney.dto.request.PocketMoneyUpdateRequest;
 import com.shbhack.studywithsol.pocketmoney.dto.response.PocketMoneyCreateResponse;
 import com.shbhack.studywithsol.pocketmoney.dto.response.PocketMoneyReadResponse;
+import com.shbhack.studywithsol.pocketmoney.dto.response.PocketMoneyUpdateResponse;
 import com.shbhack.studywithsol.pocketmoney.service.PocketMoneyService;
 import com.shbhack.studywithsol.utils.dto.response.BaseResponseDto;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +28,7 @@ public class PocketMoneyController {
 
     @ApiOperation(value ="용돈 저장")
     @PostMapping
-    public BaseResponseDto<PocketMoneyCreateResponse> save(@RequestBody @Valid PocketMoneyCreateRequest request) {
+    public BaseResponseDto<PocketMoneyCreateResponse> savePocketMoney(@RequestBody @Valid PocketMoneyCreateRequest request) {
         return BaseResponseDto.ok(pocketMoneyService.save(request));
     }
 
@@ -33,6 +36,12 @@ public class PocketMoneyController {
     @GetMapping
     public BaseResponseDto<PocketMoneyReadResponse> getPocketMoney(@RequestBody @Valid PocketMoneyReadRequest request) {
         return BaseResponseDto.ok(pocketMoneyService.getPocketMoney(request));
+    }
+
+    @ApiOperation(value ="용돈 수정")
+    @PutMapping
+    public BaseResponseDto<PocketMoneyUpdateResponse> updatePocketMoney(@RequestBody @Valid PocketMoneyUpdateRequest request) {
+        return BaseResponseDto.ok(pocketMoneyService.update(request));
     }
 
 }
