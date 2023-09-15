2767f7c05d1a942eef101d6102099489b524ce70
yeonhee <ydol2@naver.com>
2023-09-16 01:57:22 +0900

    Feat: 용돈 지출 response dto

---
+++ backend/src/main/java/com/shbhack/studywithsol/report/dto/response/SpendingReportResponse.java
@@ -0,0 +1,22 @@
+package com.shbhack.studywithsol.report.dto.response;
+
+import com.shbhack.studywithsol.report.domain.SpendingReport;
+
+public record SpendingReportResponse(
+
+
+        String content,
+
+        Double percent,
+
+        Long amount
+) {
+
+    public static SpendingReportResponse of(SpendingReport spendingReport) {
+        return new SpendingReportResponse(
+                spendingReport.content(),
+                spendingReport.percent(),
+                spendingReport.amount()
+        );
+    }
+}
