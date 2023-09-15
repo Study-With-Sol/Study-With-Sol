ae59ce27b2cbca82e08b05266139ed22a122f922
yeonhee <ydol2@naver.com>
2023-09-15 23:22:10 +0900

    Feat: 용돈 엔티티 구현

---
+++ backend/src/main/java/com/shbhack/studywithsol/pocketmoney/domain/PocketMoney.java
@@ -0,0 +1,47 @@
+package com.shbhack.studywithsol.pocketmoney.domain;
+
+import com.shbhack.studywithsol.user.domain.Connection;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PocketMoney {
+
+    /**
+     *
+     *  id : PK
+     *  connection : 부모-자녀
+     *  amount : 용돈
+     *  paymentDate : 지급일
+     *  paymentStatus : 지급상태
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "connection_id")
+    private Connection connection;
+
+    @Column
+    private Long amount;
+
+    @Column
+    private Integer paymentDate;
+
+    @Column(nullable = false)
+    private Boolean paymentStatus;
+    
+}
